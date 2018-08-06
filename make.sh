1369d01c4b95c3af6b5f90fbebd53935b1783873
Marco <marco.helmich@live.com>
2018-08-06 10:40:56 -0700

    - remove commented out line

--- make.sh
+++ make.sh
@@ -6,6 +6,5 @@ LLVM_CONFIG="${LLVM_ROOT}/bin/llvm-config"
 export CGO_CPPFLAGS="$("$LLVM_CONFIG" --cppflags)"
 export CGO_CXXFLAGS=-std=c++11
 export CGO_LDFLAGS="$("$LLVM_CONFIG" --ldflags --libs --system-libs all)"
-# export GO_EXTLINK_ENABLED=1
 
 go build -tags byollvm -v
