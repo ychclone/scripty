a0a24da87bc826c225fcf17ac9da5f8769904bfc
Marco <marco.helmich@live.com>
2018-08-07 09:26:49 -0700

    - fix path

--- third_party/antlr4/generate.sh
+++ third_party/antlr4/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -f ../../parsergen/*
-java -jar ./antlr-4.7-complete.jar -Dlanguage=Go scripty.g4 -o ../../parsergen -package parsergen
+java -jar ./antlr-4.7-complete.jar -Dlanguage=Go scripty.g4 -o ../../parser/parsergen -package parsergen
